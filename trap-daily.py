de5e8b986b98c2b1de9808a709dc0b5e34edd295
Daniel Dansker <danieldansker@gmail.com>
2022-02-06 23:06:34 +0200

    new script

--- trap-daily.py
+++ trap-daily.py
@@ -12,7 +12,7 @@ import logging
 import subprocess
 import json
 
-FOCUS_VAL = 170 # Motorized 8mp line
+FOCUS_VAL = 202 # Motorized 8mp line
 
 FAIL_REBOOT_ATTEMPTS = 2
 REBOOT_TIME = 300  # 5 minutes
@@ -23,16 +23,15 @@ URL = 'https://us-central1-cameraapp-49969.cloudfunctions.net/serverless/trap_im
 BOOT_DATA_FILE_PATH = "trap.data"
 STARTUP_TIMES = ['11:00:00', '13:00:00', '15:00:00', '17:00:00', '19:00:00', '21:00:00', '23:00:00']
 
-is_test = False
+# is_test = False
 
 # Boot data
-boot_count = None
-startup_time = None
-run_time = None
-image_taken_today = None
-should_stay_on = False
-start_time = time.time()
-
+# boot_count = None
+# startup_time = None
+# run_time = None
+# image_taken_today = None
+# should_stay_on = False
+# start_time = time.time()
 
 def connected_to_internet(url='http://www.google.com/', timeout=10):
     try:
@@ -41,7 +40,6 @@ def connected_to_internet(url='http://www.google.com/', timeout=10):
     except requests.ConnectionError:
         logging.info("No internet connection available.")
     return False
-
 
 def get_serial():
     cpu_serial = None
@@ -55,7 +53,6 @@ def get_serial():
         logging.error("Couldn't return trap's serial")
         return None
     return cpu_serial
-
 
 def get_camera_type():
     camera_five = None
@@ -77,8 +74,6 @@ def get_focus_value():
             return None
     return focus
 
-
-
 def get_token():
     token_trap = None
     if path.exists('token.db'):
@@ -89,9 +84,8 @@ def get_token():
             return None
     return token_trap
 
-
 def get_test_mode():
-    global is_test
+    # global is_test
     if path.exists('testMode.db'):
         file = open('testMode.db', "r")
         test_mode = file.read().strip()
@@ -99,13 +93,20 @@ def get_test_mode():
         if not test_mode:
             return None
         if test_mode == "true":
-            is_test = True
+            # is_test = True
             return True
         elif test_mode == "false":
-            is_test = False
+            # is_test = False
             return False
     return None
 
+def get_trap_boot_data_config():
+    if path.isfile(BOOT_DATA_FILE_PATH):
+        with open(BOOT_DATA_FILE_PATH) as file:
+            config = json.load(file)
+        return config
+    else:
+        logging.info("no boot data file")
 
 def read_trap_boot_data():
     global boot_count, startup_time, run_time, image_taken_today
@@ -125,8 +126,7 @@ def read_trap_boot_data():
         run_time = 0
         image_taken_today = False
 
-
-def write_trap_boot_data():
+def write_trap_boot_data(boot_count, startup_time, image_taken_today):
     logging.info("Boot count is " + str(boot_count))
     logging.info("Startup time is " + str(startup_time))
     file = open(BOOT_DATA_FILE_PATH, "w")
@@ -134,7 +134,6 @@ def write_trap_boot_data():
         {'boot_count': boot_count, 'startup_time': startup_time,
          'run_time': run_time, 'image_taken_today': image_taken_today}, file)
     file.close()
-
 
 def take_pic():
     is_five_mega = get_camera_type()
@@ -163,36 +162,35 @@ def take_pic():
         logging.info("Image taken and saved")
 
 
-def send_pic():
-    body, headers = get_body_and_headers()
-    if not body or not headers:
-        return
-    old_time = time.time()
-    result = wait_for_connectivity(old_time)
-    if not result:
-        return True
-    logging.info('Connected to internet')
-    result = send_request(old_time, body, headers)
-    if not result:
-        return True
-
-    if result.status_code == 200:
-        data = result.json()
-        logging.info('Image sent! response data: ' + str(data))
-        for action in data:
-            check_response_for_actions(action)
-    else:
-        logging.error("Image was not sent - " + result.text)
-
+# def send_pic():
+#     body, headers = get_body_and_headers()
+#     if not body or not headers:
+#         return
+#     old_time = time.time()
+#     result = wait_for_connectivity(old_time)
+#     if not result:
+#         return True
+#     logging.info('Connected to internet')
+#     result = send_request(old_time, body, headers)
+#     if not result:
+#         return True
+#
+#     if result.status_code == 200:
+#         data = result.json()
+#         logging.info('Image sent! response data: ' + str(data))
+#         for action in data:
+#             check_response_for_actions(action)
+#     else:
+#         logging.error("Image was not sent - " + result.text)
 
 def check_response_for_actions(data):
-    global should_stay_on, run_time
+    # global should_stay_on, run_time
     try:
         if data['action'] == "none":
             logging.info("No response action was received")
-        elif data['action'] == "stayOn":
-            logging.info("Stay on response action was received")
-            should_stay_on = stay_on()
+        # elif data['action'] == "stayOn":
+        #     logging.info("Stay on response action was received")
+        #     should_stay_on = stay_on()
         elif data['action'] == "changeBattery":
             logging.info("Change battery response action was received")
             run_time = change_battery()
@@ -202,47 +200,46 @@ def check_response_for_actions(data):
                 update(data['value'])
             else:
                 update()
-        elif data['action'] == 'log_update':
-            logging.info("Log response action was received")
-            send_log(get_token(), get_serial())
+        # elif data['action'] == 'log_update':
+        #     logging.info("Log response action was received")
+        #     send_log(get_token(), get_serial())
     except Exception as e:
         logging.exception(str(e))
 
+#
+# def get_body_and_headers():
+#     trap_id = get_serial()
+#     if not trap_id:
+#         logging.error("Fatal error no serial for pi")
+#         return
+#     logging.info('Trap serial id:' + str(trap_id))
+#     token = get_token()
+#     if not token:
+#         logging.error("Fatal error no token for pi")
+#         return
+#     test_mode = get_test_mode()
+#     if test_mode is None:
+#         logging.error("Trap is off, no test or production set")
+#         return
+#     logging.info("Mode is : " + ("production" if not test_mode else "test"))
+#     with open('latest.jpg', "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read())
+#     image_name = datetime.now().strftime("%d-%m-%Y-%H_%M") + ".jpg"
+#     number_of_boots = startup_time * FAIL_REBOOT_ATTEMPTS + boot_count
+#     body = {'image': encoded_string, 'trapId': trap_id, 'imageName': image_name, 'testMode': test_mode,
+#             'runTime': run_time + calc_run_time(), 'numberOfBoots': number_of_boots}
+#     headers = {"Authorization": "Bearer " + token}
+#     return body, headers
 
-def get_body_and_headers():
-    trap_id = get_serial()
-    if not trap_id:
-        logging.error("Fatal error no serial for pi")
-        return
-    logging.info('Trap serial id:' + str(trap_id))
-    token = get_token()
-    if not token:
-        logging.error("Fatal error no token for pi")
-        return
-    test_mode = get_test_mode()
-    if test_mode is None:
-        logging.error("Trap is off, no test or production set")
-        return
-    logging.info("Mode is : " + ("production" if not test_mode else "test"))
-    with open('latest.jpg', "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    image_name = datetime.now().strftime("%d-%m-%Y-%H_%M") + ".jpg"
-    number_of_boots = startup_time * FAIL_REBOOT_ATTEMPTS + boot_count
-    body = {'image': encoded_string, 'trapId': trap_id, 'imageName': image_name, 'testMode': test_mode,
-            'runTime': run_time + calc_run_time(), 'numberOfBoots': number_of_boots}
-    headers = {"Authorization": "Bearer " + token}
-    return body, headers
-
-
-def wait_for_connectivity(old_time):
+def wait_for_connectivity(start_of_run):
     time.sleep(CONNECTIVITY_SLEEP_TIME)
     while not connected_to_internet():
+        logging.info("Sleeping for: " + CONNECTIVITY_SLEEP_TIME)
         time.sleep(CONNECTIVITY_SLEEP_TIME)
-        if time.time() - old_time > REBOOT_TIME:
-            return False
+        if time.time() - start_of_run > REBOOT_TIME:
+            return run_reboot()
     logging.info('Connected to internet')
     return True
-
 
 def send_request(old_time, body, headers):
     while True:
@@ -258,8 +255,7 @@ def send_request(old_time, body, headers):
         else:
             return res
 
-
-def set_startup_time(start_index=startup_time):
+def set_startup_time(is_test, start_index=startup_time):
     if is_test:
         return
     p = subprocess.Popen(['sh', 'wittypi/wittyPi.sh'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -273,10 +269,9 @@ def set_startup_time(start_index=startup_time):
             logging.info(line[14:])
     logging.info("Next startup time set to: " + str(start))
 
-
 def run_reboot():
     logging.info('Run reboot')
-    global boot_count, startup_time, image_taken_today, run_time
+    # global boot_count, startup_time, image_taken_today, run_time
     run_time += calc_run_time()
     if boot_count == FAIL_REBOOT_ATTEMPTS:
         logging.info("Max reboots reached")
@@ -297,7 +292,6 @@ def run_reboot():
         logging.info("Rebooting")
         system('reboot')
 
-
 def calc_run_time():
     now = time.time()
     return (now - start_time) / 60
@@ -307,14 +301,13 @@ def configure_logging(logging):
     logger_format = '%(asctime)s.%(msecs)03d %(levelname)s : %(message)s'
     logging.basicConfig(filename="trap.log", level=logging.DEBUG, datefmt='%d-%m-%Y %H:%M:%S', format=logger_format)
 
-
 def update_trap_data(db, data):
     my_file = open(db, "w")
     logging.info("writing to :" + db +". with value: " + data)
     my_file.write(data)
     my_file.close()
 
-def send_detection(token, trap_id, test_mode):
+def send_image(token, trap_id, test_mode):
     with open('latest.jpg', "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     image_name = datetime.now().strftime("%d-%m-%Y-%H_%M") + ".jpg"
@@ -325,78 +318,95 @@ def send_detection(token, trap_id, test_mode):
     logging.info('Attempting to send request')
     return requests.post(URL, data=body, headers=headers, timeout=120)
 
+def send_detection(token, trap_id, test_mode, start_of_run):
+    send_attempt = True
+    while send_attempt:
+        try:
+            result = send_image(token, trap_id, test_mode)
+        except Exception as e:
+            time.sleep(CONNECTIVITY_SLEEP_TIME)
+            if time.time() - start_of_run > REBOOT_TIME:
+                logging.error(str(e) + " reached max retries. shutting off")
+                run_reboot()
+                return
+            logging.error(str(e) + " failed attempt at sending request")
+        else:
+            if result.status_code == 200:
+                data = result.json()
+                logging.info('Image sent! response data: ' + str(data))
+                for action in data:
+                    check_response_for_actions(action)
+                send_attempt = False
+            else:
+                logging.error("Image was not sent - " + result.text)
 
-def update_trap_status(trap_status):
+
+def update_trap_db_status(trap_status):
     if trap_status["dev_mode"]:
         update_trap_data("testMode.db", trap_status["dev_mode"])
     if trap_status["focus"]:
         update_trap_data("trap_focus.db", trap_status["focus"])
 
 
+def validate_trap_base_data(token, serial):
+    if not token:
+        logging.error("Fatal error no token for pi")
+        return False
+    if not serial:
+        logging.error("Fatal error no serial for pi")
+        return False
+
+def get_trap_base_data():
+    return get_token(), get_serial()
+
+def get_trap_boot_data(data):
+    if path.isfile(BOOT_DATA_FILE_PATH):
+        with open(BOOT_DATA_FILE_PATH) as file:
+            config = json.load(file)
+        logging.info('trap data: ' + str(config))
+        boot_data = config[data]
+        logging.info('trap boot data for: ' + data + "is: " + boot_data)
+        return boot_data
+
+def send_log_data(token, serial, weekday, trap_status, delete_log = False):
+    if trap_status["send_log"] or weekday == 6:
+        logging.info('Sending weekend log')
+        send_log(token, serial, delete_log)
+
+
 def main():
+    should_stay_on = False
     start_of_run = time.time()
-    global image_taken_today, boot_count, startup_time, run_time
-    send_attempt = True
     configure_logging(logging)
     logging.info("========================STARTING NEW WAKEUP LOG========================")
     try:
-        read_trap_boot_data()
-        token = get_token()
-        if not token:
-            logging.error("Fatal error no token for pi")
+        token, serial = get_trap_base_data()
+        if not validate_trap_base_data(token, serial):
             return
-        serial = get_serial()
-        if not serial:
-            logging.error("Fatal error no serial for pi")
-            return
-        if boot_count == 0:
-            set_startup_time(startup_time)
-
-        wait_for_connectivity(start_time)
-
+        wait_for_connectivity(start_of_run)
         trap_status = get_trap_status(token, serial)
         update_trap_status(trap_status)
-
-        if not image_taken_today:
+        if not get_trap_boot_data("image_taken_today"):
             take_pic()
 
         test_mode = get_test_mode()
         if test_mode is None:
-            logging.error("")
             return
+        if get_trap_boot_data("boot_count") == 0:
+            set_startup_time(test_mode, get_trap_boot_data("startup_time"))
         logging.info("Mode is : " + ("production" if not test_mode else "test"))
-        while send_attempt:
-            try:
-                result = send_detection(token, serial, test_mode)
-            except Exception as e:
-                time.sleep(CONNECTIVITY_SLEEP_TIME)
-                if time.time() - start_of_run > REBOOT_TIME:
-                    logging.error(str(e) + " reached max retries. shutting off")
-                    run_reboot()
-                    return
-                logging.error(str(e) + " failed attempt at sending request")
-            else:
-                if result.status_code == 200:
-                    data = result.json()
-                    logging.info('Image sent! response data: ' + str(data))
-                    for action in data:
-                        check_response_for_actions(action)
-                    send_attempt = False
-                else:
-                    logging.error("Image was not sent - " + result.text)
+        send_detection(token, serial, test_mode, start_of_run)
+        send_log_data(token, serial, datetime.today().weekday(), delete_log, trap_status)
+        should_stay_on = trap_status["stay_on"]
+
 
     except Exception as e:
         logging.exception(str(e))
-
     if should_stay_on:
         time.sleep(STAY_ON_SLEEP)
     else:
         time.sleep(SLEEP_BEFORE_SHUTDOWN)
     system("shutdown now -h")
-
-
-
-
 
 
 

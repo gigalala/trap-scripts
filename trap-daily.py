867fa85ec06bd72969d014c3d72b1b99105b5cd0
Daniel Dansker <danieldansker@gmail.com>
2022-02-10 20:09:12 +0200

    changing logs

--- trap-daily.py
+++ trap-daily.py
@@ -297,23 +297,19 @@ def main():
         logging.info("Startup time is: " + str(start_up_time))
         boot_count = get_trap_boot_data("boot_count", config)
         if boot_count == 0:
-            # start_up_time = get_trap_boot_data("startup_time", config)
             set_startup_time(test_mode, start_up_time)
         logging.info("Mode is : " + ("production" if not test_mode else "test"))
         send_detection(token, serial, test_mode, start_of_run, start_up_time, boot_count, config)
         send_log_data(token, serial, datetime.today().weekday(), trap_status, False)
         should_stay_on = trap_status["stay_on"]
-        logging.info("now - " + str(time.time()) + "strat-of-run: " + str(start_of_run) + "on time - " + str(STAY_ON_SLEEP))
         while should_stay_on and (time.time() - start_of_run) < STAY_ON_SLEEP:
-            logging.info(
-                "now - " + str(time.time()) + "strat-of-run: " + str(start_of_run) + "on time - " + str(STAY_ON_SLEEP))
-            logging.info("time diff calculation - " + str(time.time() - start_of_run))
-            logging.info("Stay on loop, sleeping for: " + str(CONNECTIVITY_SLEEP_TIME) + "seconds and sending request for changes")
+            logging.info("-----------TRAP IS STAYING ON CHECKING DATA AND PERFORMING TASKS-----------")
             time.sleep(CONNECTIVITY_SLEEP_TIME)
             changed_trap_status = get_trap_status(token, serial)
             logging.info("New changed status: " + str(changed_trap_status))
             update_trap_db_status(changed_trap_status)
             if changed_trap_status.get("take_pic"):
+                take_pic()
                 send_detection(token, serial, test_mode, start_of_run, start_up_time, boot_count, config)
             send_log_data(token, serial, datetime.today().weekday(), changed_trap_status, False)
             if changed_trap_status.get("turn_off"):

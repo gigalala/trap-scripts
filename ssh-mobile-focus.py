e4cce35968e94ed522fdea79ed67c4cfedb9b96c
gigalala <gigalala@gmail.com>
2022-06-22 14:51:51 -0700

    add try catch on camera

--- ssh-mobile-focus.py
+++ ssh-mobile-focus.py
@@ -4,14 +4,17 @@ import time
 import sys
 
 def take_pic_with_focus(focus_value):
-    camera_res = (3280, 2464)
-    arducam_vcm = CDLL('./RaspberryPi/Motorized_Focus_Camera/python/lib/libarducam_vcm.so')  # Motorized 8mp line
-    arducam_vcm.vcm_init()
-    camera = PiCamera()
-    camera.resolution = (camera_res[0], camera_res[1])
-    arducam_vcm.vcm_write(focus_value)  # Motorized 8mp line
-    time.sleep(2)  # Motorized 8mp line
-    camera.capture("latest.jpg")
+    try:
+        camera_res = (3280, 2464)
+        arducam_vcm = CDLL('./RaspberryPi/Motorized_Focus_Camera/python/lib/libarducam_vcm.so')  # Motorized 8mp line
+        arducam_vcm.vcm_init()
+        camera = PiCamera()
+        camera.resolution = (camera_res[0], camera_res[1])
+        arducam_vcm.vcm_write(focus_value)  # Motorized 8mp line
+        time.sleep(2)  # Motorized 8mp line
+        camera.capture("latest.jpg")
+    except Exception as e:
+        print(e)
 
 
 take_pic_with_focus(sys.argv[1])

77d71bda53c47e7d2a12df6fc2ecd864b69d6a17
gigalala <gigalala@gmail.com>
2022-06-30 16:55:36 -0700

    changing to test name

--- ssh-mobile-focus.py
+++ ssh-mobile-focus.py
@@ -12,7 +12,7 @@ def take_pic_with_focus(focus_value):
         camera.resolution = (camera_res[0], camera_res[1])
         arducam_vcm.vcm_write(focus_value)  # Motorized 8mp line
         time.sleep(2)  # Motorized 8mp line
-        camera.capture("latest.jpg")
+        camera.capture("test.jpg")
         print("image_taken")
     except Exception as e:
         print(e)

1b741f8104a1d02883f442d5bf622bce8a5477d5
daniel <danieldansker@gmail.com>
2023-03-13 23:03:38 +0200

    add support for new witty

--- trapInit.sh
+++ trapInit.sh
@@ -116,15 +116,15 @@ if [[ "$option" == 1 ]] ;then
     #Download and install Witty Pi software
     echo 'Downloading Witty pi software'
     read -p "Install new witty pi 4? (y/n) " yn
-    if [[ "$yn" == 'y' ]]; then
-      wget http://www.uugear.com/repo/WittyPi4/install.sh
-      echo "true" > /home/pi/new_witty.db
-      echo 'Install new Witty pi 4 Software'
-    break; fi
-    elif [[ "$yn" == 'n' ]]; then
-      wget http://www.uugear.com/repo/WittyPi3/install.sh
-      echo 'Install old Witty pi 3 software'
-    break; fi
+#    if [[ "$yn" == 'y' ]]; then
+    wget http://www.uugear.com/repo/WittyPi4/install.sh
+    echo "true" > /home/pi/new_witty.db
+    echo 'Install new Witty pi 4 Software'
+#    break; fi
+#    elif [[ "$yn" == 'n' ]]; then
+#      wget http://www.uugear.com/repo/WittyPi3/install.sh
+#      echo 'Install old Witty pi 3 software'
+#    break; fi
     sudo sh install.sh
     echo 'adding GPIO-4 fix to wittyPi/daemon.sh'
     sudo sed -i '119d' wittypi/daemon.sh # dansker

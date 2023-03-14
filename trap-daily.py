99a67d48fed5370ea5b7fdd4f6a5d2f377c875d5
daniel <danieldansker@gmail.com>
2023-03-14 21:35:57 +0200

    replace content

--- trap-daily.py
+++ trap-daily.py
@@ -547,7 +547,7 @@ def main():
         except Exception as e:
             logging.exception(str(e))
     time.sleep(SLEEP_BEFORE_SHUTDOWN)
-#    system("shutdown now -h")
+    system("shutdown now -h")
 
 if __name__ == "__main__":
     main()
